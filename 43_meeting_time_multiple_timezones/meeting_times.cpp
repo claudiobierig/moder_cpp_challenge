6fd0e6c142cf117c5cd424a3f8017499ccfcaad7
claudiobierig <claudiobierig@gmail.com>
2019-07-16 14:04:26 +0200

    Use submodule instead of ExternalProject to get example to work

--- 43_meeting_time_multiple_timezones/meeting_times.cpp
+++ 43_meeting_time_multiple_timezones/meeting_times.cpp
@@ -1,10 +1,36 @@
+/*#include "date/tz.h"
 #include <iostream>
 
-#include <date/date.h>
-
-int main(){
-    if(date::day{1}.ok()){
-        std::cout << "ok\n";
+int main()
+{
+    using namespace date;
+    using namespace std::chrono_literals;
+    try
+    {
+        auto zt = make_zoned("America/New_York", local_days{Sunday[2]/March/2016} + 2h + 30min);
+        std::cout << zt << "\n";
     }
-    std::cout << "meeting times\n";
+    catch (const nonexistent_local_time& e)
+    {
+        std::cout << e.what() << '\n';
+    }
 }
+
+*/
+#include "date/tz.h"
+#include <iostream>
+
+int
+main()
+{
+    using namespace date;
+    using namespace date::literals;
+    using namespace std::chrono_literals;
+    auto meet_nyc = make_zoned("America/New_York", date::local_days{Monday[1]/May/2016} + 9h);
+    auto meet_lon = make_zoned("Europe/London",    meet_nyc);
+    auto meet_syd = make_zoned("Australia/Sydney", meet_nyc);
+    std::cout << "The New York meeting is " << meet_nyc << '\n';
+    std::cout << "The London   meeting is " << meet_lon << '\n';
+    std::cout << "The Sydney   meeting is " << meet_syd << '\n';
+}
+ 
