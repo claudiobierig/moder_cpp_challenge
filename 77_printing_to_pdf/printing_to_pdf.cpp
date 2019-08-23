6e5bfc9392887053f8d9fdf34797534f61b0e8bc
claudiobierig <claudiobierig@gmail.com>
2019-08-23 21:08:49 +0200

    Get basic example with libharu to compile

--- 77_printing_to_pdf/printing_to_pdf.cpp
+++ 77_printing_to_pdf/printing_to_pdf.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
+#include "hpdf.h"
+
+
+
+void
+error_handler (HPDF_STATUS   error_no,
+                HPDF_STATUS   detail_no,
+                void         *user_data)
+{
+    std::cout << error_no << detail_no << user_data << std::endl;
+}
 
 int main(){
-    std::cout << "Hello" << std::endl;
+    HPDF_Doc pdf;
+
+    pdf = HPDF_New (error_handler, NULL);
+    if (!pdf) {
+        std::cout << "ERROR: cannot create pdf object.\n";
+        return 1;
+    }
+/*
+    if (setjmp(env)) {
+        HPDF_Free (pdf);
+        return 1;
+    }*/
 }
